9274e572ddd249beb96a90148ce0658ef7be1cd0
jenZ313 <jinran.zhu@mail.utoronto.ca>
2021-10-11 15:54:28 -0400

    1

---
+++ src/java/main/Test.java
@@ -0,0 +1,13 @@
+public class Test {
+
+
+}
+
+
+//    Stores all the words in a test
+//        Stores the number of the question in a test
+//        Stores the time limit for the test
+//        Stores whether the test should be supervised by a teacher
+//        Stores the score of the test
+//        Getter methods for all the above information
+//        Author (who made the test)
