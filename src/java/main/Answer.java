6c89ece9b985fdf3edf8d4d0c11fd930655906d2
DarcyWang1 <darcywang5@gmail.com>
2021-10-16 00:57:35 -0400

    fix group class

--- src/java/main/Answer.java
+++ src/java/main/Answer.java
@@ -21,5 +21,8 @@ public class Answer {
         return result;
 
     }
+    public Test getAbout(){
+        return about;
+    }
 
 }
--- src/java/main/Group.java
+++ src/java/main/Group.java
@@ -1,16 +1,22 @@
 import javafx.scene.Parent;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class Group {
     private Student[] students;
     private Teacher teacher;
     private String name;
     private int ID;
     public static final int MAXNUMBER = 30;
+    public HashMap<Test, List<Answer>> tests;
     public Group(Teacher teacher, String name){
         this.teacher = teacher;
         this.name=name;
         this.students=new Student[Group.MAXNUMBER];
         this.ID = IDcreater.creat();
+        tests=new HashMap<>();
     }
     //public Group(){
 
@@ -33,6 +39,21 @@ public class Group {
     }
     public Teacher getTeacher(){
         return this.teacher;
+    }
+    public boolean addTest(Test t){
+        if(tests.containsKey(t)){
+            return false;
+        }
+        tests.put(t,new ArrayList<>());
+        return true;
+    }
+    public boolean answerTest(Answer a){
+        if(tests.containsKey(a.getAbout())){
+            tests.get(a.getAbout()).remove(a);
+            tests.get(a.getAbout()).add(a);
+            return true;
+        }
+        return false;
     }
 
 }
