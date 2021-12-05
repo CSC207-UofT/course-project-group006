8c5ac8e5fc0c981f323a67b17e3bb16ac3eb53e2
Jerry9090 <zihua.xu@mail.utoronto.ca>
2021-12-05 15:38:45 -0500

    Added javadoc to question entity

--- src/main/java/BackEnd/Entities/Question.java
+++ src/main/java/BackEnd/Entities/Question.java
@@ -12,23 +12,47 @@ public class Question implements QuestionInterface{
         this.marks = mark;
     }
 
+    /**
+     * Getter method of question name
+     * @return Question name
+     */
     @Override
     public String getName() {
         return this.name;
     }
 
+    /**
+     * Getter method of question
+     * @return Question
+     */
     public String getQuestion(){
         return this.question;
     }
+
+    /**
+     * Method to return the score of a student do right
+     * @param ans Answer to the question
+     * @return Mark gotten by student
+     */
     public int score(String ans){
         if(!ans.equals("") &answer.contains(ans)){
             return marks;
         }
         return 0;
     }
+
+    /**
+     * Getter method of answer
+     * @return Answer to the question
+     */
     public String getAnswer(){
         return this.answer;
     }
+
+    /**
+     * Getter method of marks
+     * @return Marks of the question
+     */
     public int getMarks(){
         return this.marks;
     }
