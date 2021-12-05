a683253085983bc118735919b2fed43dfcf9772a
Jerry9090 <zihua.xu@mail.utoronto.ca>
2021-12-04 21:12:31 -0500

    process request method java doc added

--- src/main/java/Servlets/GroupPageServlet.java
+++ src/main/java/Servlets/GroupPageServlet.java
@@ -62,6 +62,14 @@ public class GroupPageServlet extends TestServlet {
         RequestDispatcher r = request.getRequestDispatcher("GroupDetail.jsp");
         r.forward(request, response);
     }
+
+    /**
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
     protected void processRequest1(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
